25517498b5090297e68ca479d2b0490af950ddb0
Danil Kucheruk <danilkucheruk8@gmail.com>
2024-01-27 22:02:34 +0300

    init

---
+++ src/test/java/com/danillkucheruk/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danillkucheruk.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
