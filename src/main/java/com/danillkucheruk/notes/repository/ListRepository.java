ac067689577e29aac1a6545360a30303c7b7eebb
Danil Kucheruk <danilkucheruk8@gmail.com>
2024-01-29 22:39:06 +0300

    Add repository for listEntity

--- src/main/java/com/danillkucheruk/notes/repository/ListRepository.java
+++ src/main/java/com/danillkucheruk/notes/repository/ListRepository.java
@@ -4,11 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.danillkucheruk.notes.model.ListEntity;
-import com.danillkucheruk.notes.model.User;
 
 import java.util.List;
 
 @Repository
 public interface ListRepository extends JpaRepository<ListEntity,Long>{
-    List<ListEntity> findByUser(User user);
+    List<ListEntity> findByUserId(Long userId);
 }
